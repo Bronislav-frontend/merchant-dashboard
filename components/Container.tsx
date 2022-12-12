1a6b668b8a3452bf539858ee57378d74a49d18f4
Bronislav-frontend <marine4bro@gmail.com>
2022-12-12 22:20:43 +0200

    Update Container.tsx

--- components/Container.tsx
+++ components/Container.tsx
@@ -8,7 +8,7 @@ const Container = ({ children }: Props) => {
   const size = useWindowSize();
   return (
     <section
-      className={`h-full md:bg-[url('../assets/bg.png')] lg:py-[51px] xl:py-0 ${
+      className={`h-screen md:bg-[url('../assets/bg.png')] lg:py-[51px] xl:py-0 ${
         size.height < 835 ? "md:py-[51px]" : "md:pb-[330px] md:pt-[132px]"
       }`}
     >
