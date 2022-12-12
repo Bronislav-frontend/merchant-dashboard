8c0cb3a38abeb66b25cebe48a459a1503076532c
Bronislav-frontend <marine4bro@gmail.com>
2022-12-12 22:41:58 +0200

    fix with bg and sidebar

--- components/DesktopComponents/LeftSidebar.tsx
+++ components/DesktopComponents/LeftSidebar.tsx
@@ -7,7 +7,7 @@ interface LeftSideBarProp {
 
 const LeftSidebar = ({ isProgressbarHidden }: LeftSideBarProp) => {
   return (
-    <div className="w-[568px] bg-gradient-to-t from-[#0D3251] to-[#19476C] pl-[102px] pb-[56px] h-screen">
+    <div className="min-h-screen h-full w-[568px] bg-gradient-to-t from-[#0D3251] to-[#19476C] pl-[102px] pb-[56px]">
       <div className="pt-[200px] mb-[172px]">
         <ProgressbarDesktop isHidden={isProgressbarHidden} />
       </div>
